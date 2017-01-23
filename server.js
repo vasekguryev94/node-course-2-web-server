d050140f5d9bc04dc4a4bd9dbdc1ee692cb96396
vasekguryev <guryevi30041994@gmail.com>
2017-01-23 16:35:35 +0200

    Add project page

--- server.js
+++ server.js
@@ -48,6 +48,12 @@ app.get('/about', (req, res) => {
   });
 });
 
+app.get('/projects', (req, res) => {
+  res.render('projects.hbs', {
+    pageTitle: 'Projects'
+  });
+});
+
 // /bad - sebd back json with errorMessage
 app.get('/bad', (req, res) => {
   res.send({
